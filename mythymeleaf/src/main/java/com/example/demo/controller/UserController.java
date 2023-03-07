d51b70cc351a2a98331bee064ce75fa87dc9ab2e
011568 <rickren@megabank.com.tw>
2023-03-07 12:49:13 +0800

    lesson 2

---
+++ mythymeleaf/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,95 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * http://localhost:8081/sts/user/add/sample/data
+     */
+    @GetMapping("/add/sample/data")
+    @ResponseBody
+    public String addSampleData() {
+        Faker faker = new Faker();
+        Random random = new Random();
+        for (int i = 0; i < 150; i++) {
+            User user = new User();
+            user.setName(faker.name().firstName());
+            user.setPassword(String.format("%04d", random.nextInt(10000))); // 0000~9999
+            user.setBirth(faker.date().birthday());
+            user.setHeight(random.nextInt(51) + 150);
+            user.setWeight(random.nextInt(20) + (int) ((user.getHeight() - 80) * 0.7));
+            userRepository.save(user);
+        }
+        return "Add Sample Data OK";
+    }
+
+    /**
+     * http://localhost:8081/sts/user/list/sample/data
+     */
+    @GetMapping("/list/sample/data")
+    @ResponseBody
+    public List<User> listSampleData() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/")
+    public String index(@ModelAttribute User user, Model model) {
+        model.addAttribute("_method", "POST");
+        model.addAttribute("users", userRepository.findAll());
+        return "user";
+    }
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("_method", "PUT");
+        model.addAttribute("user", userRepository.getById(id));
+        model.addAttribute("users", userRepository.findAll());
+        return "user";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userRepository.deleteById(id);
+        return "redirect:../";
+    }
+
+    @PostMapping("/")
+    public String add(@Valid @ModelAttribute User user, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("_method", "POST");
+            model.addAttribute("users", userRepository.findAll());
+            return "user";
+        }
+        userRepository.save(user);
+        return "redirect:./";
+    }
+
+    @PutMapping("/")
+    public String update(@Valid @ModelAttribute User user, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("_method", "PUT");
+            model.addAttribute("users", userRepository.findAll());
+            return "user";
+        }
+        // 根據 JPA 規範
+        // 若有 user.id 則進行數據更新
+        userRepository.save(user);
+        return "redirect:./";
+    }
+}
--- mythymeleaf/src/main/java/com/example/demo/entity/User.java
+++ mythymeleaf/src/main/java/com/example/demo/entity/User.java
@@ -1,13 +1,13 @@
 package com.example.demo.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
